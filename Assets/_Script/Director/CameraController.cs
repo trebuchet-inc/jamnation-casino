935ba832cd1b688e7eee19d3838111b7cd0e9339
Vincent <vbrunetd@github.com>
2017-11-10 21:52:15 -0500

    Guillaume fait de l<attitude

---
+++ Assets/_Script/Director/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public int cameraId;
+	public bool isActiveCamera;
+	private Quaternion sourceLocalRotation;
+	private Vector3 sourceLocalPosition;
+
+	[HideInInspector]
+	public CameraManager cameraManager;
+
+	// Use this for initialization
+	public void Initialize () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isActiveCamera)
+		{
+			
+			{
+				transform.rotation.eulerAngles = ;				
+			}
+
+		}
+		
+	}
+
+	void LateUpdate()
+	{
+		if (isActiveCamera)
+		{
+			cameraManager.cameraTransform.position = transform.position;
+			cameraManager.cameraTransform.localRotation = transform.localRotation;
+		}		
+	}
+
+	public void CutToThisCamera()
+	{
+		isActiveCamera = true;
+		sourceLocalPosition = transform.localPosition;
+		sourceLocalRotation = transform.localRotation;
+		cameraManager.cameraTransform.position = transform.position;
+		cameraManager.cameraTransform.localRotation = transform.localRotation;
+
+	}
+
+	public void ResetPosition()
+	{
+		transform.localRotation = sourceLocalRotation;
+		transform.localPosition = sourceLocalPosition;
+	}
+}
---
+++ Assets/_Script/Director/CameraManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+	[HideInInspector]
+	public CameraController[] cameras;
+	public CameraController activeCamera;
+	public Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		cameras = FindObjectsOfType<CameraController>();
+		InitializeCameras();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int a = CameraSwitchCheck();
+		if (a != 0)
+		{
+			bool foundMatch = false;
+			for (int i = 0; i < cameras.Length; i++)
+			{
+				if (cameras[i].cameraId == a)
+				{
+					if (activeCamera!=null)
+					{
+						activeCamera.isActiveCamera = false;
+						activeCamera.ResetPosition();
+					}
+					activeCamera = cameras[i];
+					activeCamera.CutToThisCamera();
+					foundMatch = true;
+
+				}
+				
+			}
+			if (!foundMatch)
+			{
+				Debug.Log ("Did not find matching camera");
+			}
+		}
+		
+	}
+
+	void InitializeCameras(){
+		foreach(CameraController c in cameras)
+		{
+			c.Initialize();
+			c.cameraManager = this;
+		}
+	}
+
+
+	int CameraSwitchCheck()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			return 1;
+		} else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			return 2;
+		} else if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			return 3;
+		} else if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			return 4;
+		} else if (Input.GetKeyDown(KeyCode.Alpha5))
+		{
+			return 5;
+		} else if (Input.GetKeyDown(KeyCode.Alpha6))
+		{
+			return 6;
+		} else if (Input.GetKeyDown(KeyCode.Alpha7))
+		{
+			return 7;
+		} else if (Input.GetKeyDown(KeyCode.Alpha8))
+		{
+			return 8;
+		} else if (Input.GetKeyDown(KeyCode.Alpha9))
+		{
+			return 9;
+		} else {
+			return 0;
+		}
+	}
+
+}
