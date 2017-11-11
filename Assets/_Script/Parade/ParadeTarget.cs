fcd177ac605b8b55a189c45af87d8772b3489f30
Alexandre <alexandre@trebuchet.fun>
2017-11-11 15:21:22 -0500

    wowowofff

--- Assets/_Script/Parade/ParadeTarget.cs
+++ Assets/_Script/Parade/ParadeTarget.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ParadeTarget : MonoBehaviour 
 {
 	private void OnTriggerEnter(Collider other)
 	{
-		// CHECK IF PLAYER IS HERE
-		if (!GameRefereeManager.Instance.paradePhase.isReady)
+		if (other.CompareTag("Mount"))
 		{
-			GameRefereeManager.Instance.paradePhase.SetReady();
+			if (!GameRefereeManager.Instance.paradePhase.isReady)
+			{
+				GameRefereeManager.Instance.paradePhase.SetReady();
+			}
 		}
 	}
 }
