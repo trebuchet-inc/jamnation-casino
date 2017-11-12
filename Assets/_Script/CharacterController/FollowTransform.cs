3eff5ae973700783d39fd3564fff9a045bc5fcd9
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-11-11 20:22:40 -0500

    squirel

---
+++ Assets/_Script/CharacterController/FollowTransform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour 
+{
+	public Transform target;
+	public float speed;
+
+	Rigidbody _rb;
+
+	void Start()
+	{
+		_rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () 
+	{
+		Vector3 dir = target.position - transform.position;
+		_rb.velocity = dir * Time.deltaTime * speed;
+	}
+}
