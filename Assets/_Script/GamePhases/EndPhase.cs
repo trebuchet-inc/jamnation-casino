9349ac7d00989d8ebada7310d5bf365d51f70374
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-11-12 01:49:00 -0500

    squirel

--- Assets/_Script/GamePhases/EndPhase.cs
+++ Assets/_Script/GamePhases/EndPhase.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EndPhase : GamePhase
 {
 	public int endDuration;
+
+	public event Action OnReset;
 	
 	public override void StartPhase()
 	{
@@ -14,6 +17,7 @@ public class EndPhase : GamePhase
 	public override void TerminatePhase()
 	{
 		if(GameRefereeManager.Instance.isFirstGame) GameRefereeManager.Instance.isFirstGame = false;
+		if(OnReset != null) OnReset.Invoke();
 	}
 	
 	public IEnumerator RestartTimer()
