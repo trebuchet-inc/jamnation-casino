75692cabcea840c2abab20d33466ace9ee83a59b
Vincent <vbrunetd@github.com>
2017-11-12 13:46:48 -0500

    Cest la folie

--- Assets/_Script/Director/CharacterAnimatorController.cs
+++ Assets/_Script/Director/CharacterAnimatorController.cs
@@ -4,13 +4,77 @@ using UnityEngine;
 
 public class CharacterAnimatorController : MonoBehaviour {
 
+	private Animator chanim;
+	private int hypeLevel;
+	private int nextHypeLevel;
+	private float delay;
+	private bool hypeChange;
+
+
 	// Use this for initialization
-	void Start () {
+	public void Initialize () {
+		RollModelAndGetAnimator();
+		hypeLevel = Random.Range(1,5);
+		delay = 5f;
+		chanim.SetTrigger(hypeLevel.ToString());
+		
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (hypeChange)
+		{
+			if (delay <= 0)
+			{
+				hypeLevel = nextHypeLevel;
+				SetNextLevel(hypeLevel);
+				hypeChange = false;
+			}
+		}
+		else if (delay <= 0 && hypeLevel > 1)
+		{
+			hypeLevel --;
+			chanim.SetTrigger(hypeLevel.ToString());
+			delay = 5f;
+		}
+
+		delay =- Time.deltaTime;
+
 		
+	}
+
+	public void SetNextLevel (int whatIsTheHype)
+	{
+		delay = Random.Range(0,2.0f);
+		hypeChange = true;
+		nextHypeLevel = whatIsTheHype;
+	}
+
+	private void SetHypeLevel(int whatIsTheHype)
+	{
+		delay = 5f;
+		hypeLevel = whatIsTheHype + Random.Range(-1,2);
+		hypeLevel = Mathf.Clamp(hypeLevel,1,5);
+		chanim.SetTrigger(hypeLevel.ToString());
+	}
+
+
+
+	void RollModelAndGetAnimator()
+	{
+		int who = Random.Range(0,3);
+		for (int i = 0; i < 3; i++)
+		{
+			if (i != who)
+			{
+				transform.GetChild(i).gameObject.SetActive(false);
+			}		
+			else 
+			{
+				chanim = transform.GetChild(i).GetComponentInChildren<Animator>();
+			}	
+		}
+
 	}
 }
