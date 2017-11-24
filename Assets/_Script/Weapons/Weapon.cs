0684616fff66e007a9285c93614fee0e9252a6ba
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-11-24 11:59:34 -0500

    weapon-choice-debuging

--- Assets/_Script/Weapons/Weapon.cs
+++ Assets/_Script/Weapons/Weapon.cs
@@ -10,30 +10,33 @@ public class Weapon : MonoBehaviour
     public float lerpSpeed = 20;
     public GameObject[] JoinedObjects;
 
-    NVRHand weaponHand;
+    NVRHand _weaponHand;
+    NVRHand _hand;
     bool _initialized;
 
     public void Initialize(NVRHand hand)
     {
         _initialized = true;
         if(target == null) target = transform;
-        weaponHand = hand;
+        _weaponHand = hand;
         transform.parent = hand.transform.parent;
-        target.localPosition = weaponHand.transform.localPosition;
-        target.localRotation = weaponHand.transform.localRotation;
+        target.localPosition = _weaponHand.transform.localPosition;
+        target.localRotation = _weaponHand.transform.localRotation;
 
         foreach(GameObject obj in JoinedObjects)
         {
             obj.transform.parent = null;
         }
+
+        GameRefereeManager.Instance.intermissionPhase.OnRoundReset += DestroyWeapon;
     }
 
     void Update()
     {
         if(!_initialized) return;
 
-        target.localPosition = Vector3.Lerp(transform.localPosition, weaponHand.transform.localPosition, Time.deltaTime * lerpSpeed);
-        target.localRotation = Quaternion.Lerp(transform.localRotation, weaponHand.transform.localRotation, Time.deltaTime * lerpSpeed);
+        target.localPosition = Vector3.Lerp(transform.localPosition, _weaponHand.transform.localPosition, Time.deltaTime * lerpSpeed);
+        target.localRotation = Quaternion.Lerp(transform.localRotation, _weaponHand.transform.localRotation, Time.deltaTime * lerpSpeed);
     }
 
 	private void OnTriggerEnter(Collider other)
@@ -44,31 +47,33 @@ public class Weapon : MonoBehaviour
             SoundManager.Instance.PlayHit(gameObject.name);
         }
 
-
-		weaponHand = other.attachedRigidbody.GetComponent<NVRHand>();
+        if(!_initialized && other.transform.tag == "Hand") _hand = other.attachedRigidbody.GetComponent<NVRHand>();
 	}
 
 	private void OnTriggerStay(Collider other)
 	{
-		if ((object) weaponHand != null && !GameRefereeManager.Instance.weaponSelectionPhase.isWeaponChosen && weaponHand.HoldButtonDown)
+		if ((object) _hand != null && !GameRefereeManager.Instance.weaponSelectionPhase.isWeaponChosen && _hand.HoldButtonDown)
 		{
 			GameRefereeManager.Instance.weaponSelectionPhase.ChooseWeapon(ID);
-            Initialize(weaponHand);
+            Initialize(_hand);
 			SoundManager.Instance.WeaponSelected();
 		}
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
-		if((object) weaponHand != null) weaponHand = null;
+		if(!_initialized && other.transform.tag == "Hand" && (object) _hand != null) _hand = null;
 	}
 
-    void OnDestroy()
+    void DestroyWeapon()
     {
-        target = null;
+        _initialized = false;
+
         foreach(GameObject obj in JoinedObjects)
         {
             Destroy(obj);
         }
+        
+        Destroy(gameObject);
     }
 }
