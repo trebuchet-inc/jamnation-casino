8377b119f27842d6a890fe8513c9aa95eef6bc03
Master Of Evil <guillaume.duquesne@outlook.fr>
2017-11-28 15:49:14 -0500

    tweak collision & joust hit

--- Assets/_Script/CharacterController/PupetPart.cs
+++ Assets/_Script/CharacterController/PupetPart.cs
@@ -7,6 +7,8 @@ public class PupetPart : MonoBehaviour
 {
 	Rigidbody _rb;
 	BoxCollider _col;
+	MeshRenderer[] _models;
+	int baseLayer;
 
 	public event Action OnKilled;
 	public event Action OnRevived; 
@@ -15,14 +17,23 @@ public class PupetPart : MonoBehaviour
 	{
 		_rb = GetComponent<Rigidbody>();
 		_col = GetComponentInChildren<BoxCollider>();
+		_models = GetComponentsInChildren<MeshRenderer>();
+		baseLayer = _models[0].gameObject.layer;
 	}
 
 	public void Kill(Vector3 force)
 	{
 		transform.parent = null;
+
 		_rb.isKinematic = false;
 		_rb.useGravity = true;
-		if(_col != null)_col.isTrigger = false;
+		_col.isTrigger = false;
+
+		foreach(MeshRenderer renderer in _models)
+		{
+			renderer.gameObject.layer = 0;
+		}
+
 		_rb.AddForce(force, ForceMode.Impulse);
 		if(OnKilled != null) OnKilled.Invoke();
 	}
@@ -30,9 +41,16 @@ public class PupetPart : MonoBehaviour
 	public void Revive(Transform parent)
 	{
 		transform.parent = parent;
+
 		_rb.isKinematic = true;
 		_rb.useGravity = false;
-		if(_col != null) _col.isTrigger = true;
+		_col.isTrigger = true;
+
+		foreach(MeshRenderer renderer in _models)
+		{
+			renderer.gameObject.layer = baseLayer;
+		}
+
 		if(OnRevived != null) OnRevived.Invoke();
 	}
 }
